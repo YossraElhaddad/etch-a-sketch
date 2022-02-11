33e12e05b57496fe35f6db5e4a94d01fec4f03ea
YossraElHaddad <nspecial56@gmail.com>
2022-02-11 13:51:41 +0200

    Fix an error in sketch generating and adding variables in css file

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 let container = document.querySelector('#container');
+const button = document.querySelector('button');
+var root = document.querySelector(':root');
 
 const initialSize = 16 * 16;
 
 function createInitialGrid() {
     for (let i = 0; i < initialSize; i++) {
         let grid = document.createElement('div');
-        grid.classList.add('grid-white');
         grid.setAttribute('style', 'border: 1px dotted black; padding: 16px; background-color: white;');
         container.appendChild(grid);
     }
-    container.setAttribute('style', 'grid-template-columns: repeat(16, auto);');
+
 }
+
+createInitialGrid();
+
+function clearContainer() {
+    let first = container.firstElementChild;
+    while (first) {
+        first.remove();
+        first = container.firstElementChild;
+    }
+}
+
+function createSketch(length) {
+
+    clearContainer();
+    const size = length * length;
+    for (let i = 0; i < size; i++) {
+        let grid = document.createElement('div');
+        grid.setAttribute('style', 'border: 1px dotted black; padding: 16px; background-color: white;');
+        container.appendChild(grid);
+    }
+    root.style.setProperty('--columns', length);
+    root.style.setProperty('--rows', length);
+
+}
+
+function checkLength(length) {
+    if (length > 100) {
+        alert("Can't create sketch of size more than (100 x 100)!");
+        length = 100;
+    }
+}
+
+button.addEventListener('click', () => {
+    const dimension = +prompt("Enter the number of squares of a side in the sketch (up to 100 maximum)", "16");
+    checkLength(dimension);
+    createSketch(dimension);
+});
